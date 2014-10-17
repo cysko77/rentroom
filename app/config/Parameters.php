2f1a1d4fa10318ab1702e24582a28d954a8a6d64
cysko77 <cysko77@users.noreply.github.com>
2014-10-17 18:14:57 +0200

    test master

---
+++ app/config/Parameters.php
@@ -0,0 +1,9 @@
+<?php 
+
+$parameters = array('connect' => array(	'host'		=>	'localhost',
+								'type'		=>	'mysql',
+								'db'		=>	'BDDName',
+								'user'		=>	'user',
+								'password'	=>	'passedf'
+							));
+ ?>
